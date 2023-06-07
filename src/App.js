fd1456a68510d40233e4269c29a76cec32c67dfe
Bruno Bessa <beduardo2014@gmail.com>
2023-06-07 20:55:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App () {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState(0);
+
+  const [user, setUser] = useState('');
+
+  function handleRegister(e) {
+    e.preventDefault();
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    })
+  }
+
+  return(
+    <div>
+      <h1> Cadastrando Usuario</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome: </label> <br/>
+        <input 
+        placeholder='Digite seu nome' 
+        value={nome}
+        onChange={ (e) => setNome(e.target.value) }
+        /><br/>
+
+        <label>Email: </label> <br/>
+        <input placeholder='Digite seu nome' 
+        value={email}
+        onChange={ (e) => setEmail(e.target.value) }/><br/>
+
+        <label>Idade </label> <br/>
+        <input placeholder='Digite seu nome'
+        value={idade}
+        onChange={ (e) => setIdade(e.target.value) }/><br/>
+
+
+        <button type="submit">Resgistrar</button>
+      </form>
+
+    <br/><br/>
+
+      <div>
+        <span>Bem vindo: {user.nome}</span><br/>
+        <span>Idade: {user.idade}</span><br/>
+        <span>Email: {user.email}</span><br/>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Nome.js
@@ -0,0 +1,7 @@
+function Nome({ aluno, idade }) {
+    return(
+    <span>Bem-vindo: {aluno} - idade: {idade} anos</span>
+    )
+} 
+
+export default Nome;
